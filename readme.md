a9824421557bbeb5dbab66a62fe36c1907078e64
adhesive <_@qq.com>
2024-09-01 02:57:35 +0800

    init

---
+++ readme.md
@@ -0,0 +1,20 @@
+
+#### 背景
+
+[任务跟踪器 CLI (roadmap.sh)](https://roadmap.sh/projects/task-tracker#set-up-your-development-environment)
+
+#### 依赖
+
+`npm i pkg -g`
+
+
+
+#### 参考资源
+
+关于命令行 字体/背景 颜色：
+- [如何在 JS 控制台日志输出中添加颜色？前言 为了使控制台长输出更容易查看，我们可以为 JavaScript 的控制台日 - 掘金 (juejin.cn)](https://juejin.cn/post/7030623928120770591)
+- [ANSI转义代码(ansi escape code) - 知乎 (zhihu.com)](https://zhuanlan.zhihu.com/p/570148970)
+
+打包 `.js` 文件
+- [pkg - npm (npmjs.com)](https://www.npmjs.com/package/pkg)
+
---
+++ task-tracker.cjs
@@ -0,0 +1,128 @@
+// 命令行参数
+// 注：数组 process.argv 的前两项分别为 node.exe 或 当前js文件 的绝对路径
+let args = process.argv.slice(2)
+
+// 文件相关
+let fs = require('fs')
+let DATAPATH = './data.json'
+if (!fs.existsSync(DATAPATH)) {
+  fs.writeFileSync(DATAPATH, '[]')
+}
+
+let err = (msg) => {
+  msg = msg || 'error'
+  console.log(`\x1B[31merror: ${msg}\x1B[37m`)
+  // console.log(`\x1B[31m\x1B[41merror: ${msg}\x1B[37m\x1B[40m`)
+  process.exit()
+}
+
+
+fs.readFile(DATAPATH, 'utf8', (err_, data) => {
+  data = JSON.parse(data)
+
+  let funcs = {
+    save: () => fs.writeFileSync(DATAPATH, JSON.stringify(data)),
+    query: (x) => data
+      .map((v, i) => ({...v, _idx: i}))
+      .filter(v => {
+      for (let k in x) {
+        if (x[k] && x[k] !== v[k]) {
+          return false
+        }
+      }
+      return true
+    })
+  }
+
+  let cmd_handler = {
+    add: () => {
+      if (!args[1]) {
+        err('缺少参数 description')
+      }
+      let desc = args[1]
+      let stamp = new Date().getTime()
+      data.push({
+        id: stamp,
+        description: desc,
+        status: 'todo',
+        createdAt: stamp,
+        updateAt: stamp
+      })
+      funcs.save()
+    },
+    update: () => {
+      if (!args[1] || !args[2]) {
+        err('缺少参数 id 或 description')
+      }
+      let id = parseInt(args[1]), desc = args[2]
+
+      for (let v of funcs.query({ id })) {
+        data[v._idx].description = desc
+        data[v._idx].updateAt = new Date().getTime()
+      }
+      funcs.save()
+    },
+    delete: () => {
+      if (!args[1]) {
+        err('缺少参数 id')
+      }
+      let id = parseInt(args[1])
+
+      for (let v of funcs.query({ id })) {
+        data = data.filter((w, i) => v._idx !== i)
+      }
+      funcs.save()
+    },
+    'mark-in-progress': () => {
+      if (!args[1]) {
+        err('缺少参数 id')
+      }
+      let id = parseInt(args[1])
+
+      for (let v of funcs.query({ id })) {
+        data[v._idx].status = 'in-progress'
+        data[v._idx].updateAt = new Date().getTime()
+      }
+      funcs.save()
+    },
+    'mark-done': () => {
+      if (!args[1]) {
+        err('缺少参数 id')
+      }
+      let id = parseInt(args[1])
+      
+      for (let v of funcs.query({ id })) {
+        data[v._idx].status = 'done'
+        data[v._idx].updateAt = new Date().getTime()
+      }
+      funcs.save()
+    },
+    list: () => {
+      let st = args[1]
+      if (!st) {
+        console.log(data)
+      } else if(['todo', 'in-progress', 'done'].find(v => v===st)){
+        console.log(funcs.query({status: st}))
+      } else {
+        err(`无效参数: ${st}`)
+      }
+    }
+  }
+  // 别名
+  cmd_handler.del = cmd_handler.delete
+  cmd_handler.up = cmd_handler.update
+  cmd_handler.ls = cmd_handler.list
+
+
+  // 
+  let cmd = args[0]
+  if (!cmd) {
+    err('至少要有一个参数')
+  }
+  if (!cmd_handler[cmd]) {
+    err('没有此命令')
+  }
+  // 执行命令
+  cmd_handler[cmd]()
+})
+
